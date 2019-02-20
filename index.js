87b999156db09d0076a1d39b8c1ec04a4623c624
James Clement <jameseclement@gmail.com>
2019-02-20 13:15:42 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+function getFirstSelector (selector){
+  return document.querySelector(`${selector}`)
+}
+
+function nestedTarget (){
+  return document.querySelector(".target")
+}
+
+function increaseRankBy(n){
+  let rankedLists = document.querySelectorAll(".ranked-list")
+  for (let i=0; i<rankedLists.length; i++){
+    let children = rankedLists[i].children
+      for (let j=0; j<children.length; j++){
+        children[j].innerHTML = parseInt(children[j].innerHTML) + n
+      }
+    }
+  }
+
+  function deepestChild(){
+    return document.querySelector("#grand-node div div div div")
+  }
